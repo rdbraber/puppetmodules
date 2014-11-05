d8cca055b8fb99dc03709b58a979d6deaa11938b
Rob den Braber <rob@denbraber.org>
2014-11-05 01:58:25 +0100

    Add 3 and 4 as Testing and 5 and 6 as Production

--- custom-facts/lib/facter/stage.rb
+++ custom-facts/lib/facter/stage.rb
@@ -9,8 +9,10 @@
 # character     location        stage
 # 1             Madrid          Development
 # 2             Rome            Development
-# 3             Madrid          Production
-# 4             Rome            Production
+# 3             Madrid          Testing
+# 4             Rome            Testing
+# 5				Madrid			Production
+# 6				Rome			Production
 
 Facter.add(:stage) do
   setcode do
@@ -18,6 +20,8 @@ Facter.add(:stage) do
     if host[3,1] == "1" or host[3,1] == "2" 
       "Development"
     elsif host[3,1] == "3" or host[3,1] == "4" 
+     "Testing"
+    elsif host[3,1] == "5" or host[3,1] == "6" 
      "Production"
     end
   end
